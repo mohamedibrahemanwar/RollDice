7846508856e8a27a5b44150c182801fd1d464749
Mohamed Ibrahem <mohamedibrahemofficial@gmail.com>
2023-01-18 04:35:57 +0200

    RollDice App

--- app/src/main/java/com/example/rolldice/MainActivity.kt
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -5,21 +5,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import com.example.rolldice.R
+import com.example.myapplication222.R
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imageview : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btn : Button = findViewById(R.id.btn)
         btn.setOnClickListener {
+            
             Rolldice()
         }
+        imageview = findViewById(R.id.Roll)
     }
 
     private fun Rolldice() {
-        val img : ImageView = findViewById(R.id.Roll)
         val randowm = Random().nextInt(6)+1
         val x = when(randowm){
             1 -> R.drawable.dice_1
@@ -29,6 +31,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        img.setImageResource(x)
+        imageview.setImageResource(x)
     }
 }
